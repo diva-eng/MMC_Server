11a3aac5aeb99af2d9e9bffdabc908961767dd09
Siyuan Gao <r1cebank@yahoo.co.jp>
2014-04-10 16:34:33 +0900

    FEA : Implementing MDX API

---
+++ Mock/MDXLib Test/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MDXLib.API;
+
+namespace MDXLib_Test
+{
+    public partial class Form1 : Form
+    {
+        private MDXParser parser;
+        public Form1()
+        {
+            InitializeComponent();
+            parser = new MDXParser("dasdas");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = parser.GetFile(textBox1.Text);
+        }
+    }
+}
---
+++ Mock/MDXLib/MDXParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+
+namespace MDXLib.API
+{
+    public class MDXParser
+    {
+        public string userAPICode;
+        private Uri serverAddress = new Uri("http://motiondex.com/");
+        HttpClient client;
+        public MDXParser(string userAPI)
+        {
+            client = new HttpClient();
+            userAPICode = userAPI;
+        }
+        public string GetFile(string api)
+        {
+            var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string,string>("apicode", userAPICode),
+                    new KeyValuePair<string,string>("apiid", api)
+                });
+            var result = client.PostAsync(new Uri(serverAddress, RequestType.FileFromAPI.ToString()), content).Result;
+            result.EnsureSuccessStatusCode();
+            return result.Content.ReadAsStringAsync().Result;
+        }
+    }
+    public enum RequestType
+    {
+        FileFromAPI = "fileFromID.php"
+    }
+}
