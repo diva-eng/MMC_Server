4e0cfd655b79fd7b3e96d473755f097dd57568eb
Siyuan Gao <r1cebank@yahoo.co.jp>
2014-04-11 14:24:04 +0900

    FEA : MMCMessage for message between peers

---
+++ Mock/MMCLib/MMCMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+using Lidgren.Network;
+
+namespace MMC
+{
+    public class MMCMessage
+    {
+        public IPEndPoint sender;
+        public IPEndPoint reciever;
+        public ReturnStatus status;
+        public DataType[] type;
+        public string stringData;
+        public byte[] byteData;
+        public MMCControl controlData;
+        public MMCSong songData;
+        public MMCCharacter characterData;
+    }
+    public class MMCControl
+    {
+    }
+    public class MMCSong
+    {
+    }
+    public class MMCCharacter
+    {
+    }
+    public enum ReturnStatus
+    {
+        OK,
+        ERROR,
+        RECIEVED
+    }
+    public enum DataType
+    {
+        STRING,
+        BYTE,
+        CONTROL,
+        SONG,
+        CHARACTER
+    }
+}
