fe70f2fadf3746a3438831685d71ef71b770986d
Siyuan Gao <r1cebank@yahoo.co.jp>
2014-04-11 14:52:01 +0900

    FEA : Implementation of MMCCharacter

--- Mock/MMCLib/MMCMessage.cs
+++ Mock/MMCLib/MMCMessage.cs
@@ -26,9 +26,34 @@ namespace MMC
     }
     public class MMCSong
     {
+        public string Name;
+        public string SongAPI;
+        public List<string> Characters;
     }
     public class MMCCharacter
     {
+        public string Name;
+        public string ModelAPI;
+        public string MotionAPI;
+        public bool UseClientModel;
+        public bool UseClientMotion;
+        public bool UseClientRender;
+        private string ID;
+        public void Finalize()
+        {
+            if (ID != null)
+                ID = Base64Encode((Guid.NewGuid().ToString() + ("-" + Name + "-" + this.GetHashCode())));
+        }
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
     }
     public enum ReturnStatus
     {
